84b4aa98847dd0552d7130674fa3099549842003
paclaraujo <paclaraujo@gmail.com>
2019-05-22 17:19:02 -0300

    (firebase): add firebase config

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBeFO04cLk_1NV7gp4ZUmQ5-wgjOuMXwWk",
+    authDomain: "burguer-queen-lab.firebaseapp.com",
+    databaseURL: "https://burguer-queen-lab.firebaseio.com",
+    projectId: "burguer-queen-lab",
+    storageBucket: "burguer-queen-lab.appspot.com",
+    messagingSenderId: "979868558919",
+    appId: "1:979868558919:web:523d4330ee75b59a"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
