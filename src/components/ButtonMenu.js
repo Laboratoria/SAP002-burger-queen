87ae58c47b2112cb034526d1cea08378e34301d1
layshidani <lays.hidani@gmail.com>
2019-06-03 15:58:59 -0300

    Create Button component

---
+++ src/components/ButtonMenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Button.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+function ButtonMenu(props) {
+  return (
+    <button className="button buttonMenu">
+      <FontAwesomeIcon icon={props.iconName} /> {props.text} - R${props.price}
+    </button>
+  );
+}
+
+export default ButtonMenu;
