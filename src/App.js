03b64a33b0fafa19bf284b17f0e67958349e02bc
Iana-Neri22 <iananeri@outlook.com>
2019-06-03 23:45:28 -0300

    email and password inputs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import './App.css';
 import firebase from "./firebaseConfig";
+import Button from "./components/Button"
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: "",
+      password: "",
+      userType: ""
+    };
+  }
+
+  handleChange = (event, element) => {
+    const newState = this.state;
+    newState[element] = event.target.value
+    this.setState(newState);
+  }
+
+  handleClick = () => {
+    this.setState({
+      userType: {}
+    })
+    alert(this.state.email)
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>#partiuBurgerQueen</h1>
+          <input value={this.state.email} 
+            placeholder="Digite seu email"
+            onChange={(e) => this.handleChange(e, "email")}
+          />
+          <input value={this.state.password} 
+            placeholder="Digite sua senha"
+            onChange={(e) => this.handleChange(e, "password")}
+          />
+          <Button text="clique aqui" onClick={this.handleClick}/>
         </header>
       </div>
-    );
+    )
   }
-}
+  }
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import './Button.css';
+
+function Button(props) {
+  return (
+    <button className="button" onClick={props.onClick}>
+      {props.text}
+    </button>
+  );
+}
+
+export default Button;
