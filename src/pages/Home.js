1942f57d207f38384d3fc20a2a4f13b223b88b55
Iana-Neri22 <iananeri@outlook.com>
2019-06-08 04:49:45 -0300

    react tab working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,10 @@ import Button from "../components/Button"
 import Input from "../components/Input"
 import withFirebaseAuth from 'react-with-firebase-auth';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const firebaseAppAuth = firebase.auth();
-// const database = firebase.firestore();
+const database = firebase.firestore();
 
 class Home extends React.Component {
   constructor(props) {
@@ -44,44 +45,55 @@ class Home extends React.Component {
 
     return (
       <div>
-        
-        <header className="forms"> 
-        <div className="nav-container">
-            <div className="nav-link-one">Login</div>
-            <div className="nav-link-two">Criar Conta</div>
-            </div>    
-          <Input 
-            type="text" 
-            value={firstName} 
-            placeholder="Nome"
-            onChange={(e) => this.handleChange(e, "firstName")}
-         />
-         <Input 
-            type="text" 
-            value={surname} 
-            placeholder="Sobrenome"
-            onChange={(e) => this.handleChange(e, "surname")}
-         />
-        <select className="input-box">
-			    <option value="" selected disabled hidden>Setor</option>
-			    <option value="kitchen" onChange = {(e) => this.handleChange(e, "userType")}>Cozinha</option>
-			    <option value="salon" onChange = {(e) => this.handleChange(e, "userType")}>Salão</option>
-			  </select>
-        <Input 
-          type="email" 
-          value={email} 
-          placeholder="Digite seu email"
-          onChange={(e) => this.handleChange(e, "email")} 
-        />
-        <Input 
-          type="password" 
-          value={password} 
-          onChange={(e) => this.handleChange(e, "password")} 
-          placeholder="Digite sua senha"
-        />
-        <Button text="Criar Conta" onClick={this.createUser}/>
-        </header>
-      </div>
+        <Tabs>
+          <header className="forms"> 
+          <div className="nav-container">
+          <TabList>
+            <Tab> 
+              <div className="nav-link-one">Login</div>
+            </Tab>
+            <Tab>   
+              <div className="nav-link-two">Criar Conta</div></Tab>
+          </TabList>
+          </div> 
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+          <TabPanel> 
+            <Input 
+              type="text" 
+              value={firstName} 
+              placeholder="Nome"
+              onChange={(e) => this.handleChange(e, "firstName")}
+            />
+            <Input 
+              type="text" 
+              value={surname} 
+              placeholder="Sobrenome"
+              onChange={(e) => this.handleChange(e, "surname")}
+            />
+            <select className="input-box">
+			        <option value="" selected disabled hidden>Setor</option>
+			        <option value="kitchen" onChange = {(e) => this.handleChange(e, "userType")}>Cozinha</option>
+			        <option value="salon" onChange = {(e) => this.handleChange(e, "userType")}>Salão</option>
+			      </select>
+            <Input 
+              type="email" 
+              value={email} 
+              placeholder="Digite seu email"
+              onChange={(e) => this.handleChange(e, "email")} 
+            />
+           <Input 
+             type="password" 
+             value={password} 
+             onChange={(e) => this.handleChange(e, "password")} 
+             placeholder="Digite sua senha"
+           />
+           <Button text="Criar Conta" onClick={this.createUser}/>
+          </TabPanel>  
+          </header>
+         </Tabs>
+        </div>
     )
   }
 }
