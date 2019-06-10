95fbe05c35610bf60e72f46eff54d75e8490567c
TATIANE TABATA <tatit@br.ibm.com>
2019-06-10 14:08:59 -0300

    pagina de login

---
+++ src/components/Kitchen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Kitchen extends React.Component {
+    render() {
+        return (
+          <div>
+              <h1>Bem vindo a Cozinha!</h1>
+              </div>
+        )
+}
+}
+
+export default Kitchen;
--- src/components/Salao.js
+++ src/components/Salao.js
@@ -1,70 +1,129 @@
 import React from 'react';
-import '../App.css';
-import Button from '../Button'
-import firebase from '../firebaseConfig';
-// import { database } from 'firebase';
-import withFirebaseAuth from 'react-with-firebase-auth';
-import {BrowserRouter as Router, Route, Redirect, Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
-const firebaseAppAuth = firebase.auth();
-const database = firebase.firestore();
+const menu = {
+  "breakfast": [
+    { item: "Café Americano", preco: 5 },
+    { item: "Café com Leite", preco: 7 },
+    { item: "Sanduíche de Presunto e Queijo", preco: 10 },
+    { item: "Suco de Fruta Natural", preco: 7 }
+  ],
+  "lunch": [
+    { item: "Hamburguer Simples Bovino", preco: 10 },
+    { item: "Hamburguer Simples de Frango", preco: 10 },
+    { item: "Hamburguer Simples Vegetariano", preco: 10 },
+    { item: "Hamburguer Duplo Bovino", preco: 15 },
+    { item: "Hamburguer Duplo Frango", preco: 15 },
+    { item: "Hamburguer Duplo Vegetariano", preco: 15 },
+    { item: "Queijo", preco: 1 },
+    { item: "Ovo", preco: 1 },
+    { item: "Batata Frita", preco: 5 },
+    { item: "Anéis de Cebola", preco: 5 },
+    { item: "Água 500ml", preco: 5 },
+    { item: "Água 750ml", preco: 7 },
+    { item: "Bebida gaseificada 500ml", preco: 7 },
+    { item: "Bebida gaseificada 750ml", preco: 10 }
+  ]
+}
 
-class App extends React.Component {
+
+
+class Salao extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      order: "",
-      food: "",
-      listItem: []
-    };
-  }
-
-  componentDidMount() {
-    database.collection('laboratoria').get()
-      .then((querySnapshot) => {
-        const data = querySnapshot.docs.map(doc => doc.data());
-        this.setState({listItem: data});
-      });
-  }
-
-  handleChange = (event, element) => {
-    const newState = this.state;
-    newState[element] = event.target.value
-    this.setState(newState);
-  }
-
-  handleClick = () => {
-    const object = {
-      email: this.state.email,
-      password: this.state.password
+      comprar: []
     }
-    database.collection('laboratoria').add(object)
-    this.setState({
-      listItem: this.state.listItem.concat(object)
-    })
   }
 
-  
+  cliqueDaCompra = (opcao) => {
+    const itemIndex = this.state.comprar.findIndex((produto) => {
+      return produto.item === opcao.item;
+    })
+    if (itemIndex < 0) {
+      const newItem = {
+        ...opcao,
+        quantidade: 1
+      };
+      this.setState({
+        comprar: this.state.comprar.concat(newItem)
+      });
+    } else {
+      let newComprar = this.state.comprar;
+      newComprar[itemIndex].quantidade += 1;
+      this.setState({
+        comprar: newComprar
+      })
+    }
+  }
+
+  cliqueDeleta = (opcao) => {
+    const itemIndex = this.state.comprar.findIndex((produto) => {
+      return produto.item === opcao.item;
+    });
+    let newComprar = this.state.comprar;
+    newComprar[itemIndex].quantidade -= 1;
+
+    const quantidade = newComprar[itemIndex].quantidade;
+
+    if (quantidade > 0) {
+      this.setState({
+        comprar: newComprar
+      });
+    } else {
+      newComprar.splice(itemIndex, 1);
+      this.setState({
+        comprar: newComprar
+      });
+    }
+  }
+
   render() {
+    const valorTotal = this.state.comprar.reduce((acc, cur) => {
+      return acc + (cur.quantidade * cur.preco)
+    }, 0);
+    console.log(this.props)
     return (
-      <div>
-          <input value={this.state.order}
-            placeholder="Digite seu pedido"
-            onChange={(e) => this.handleChange(e, "order")} />
-          <input value={this.state.food}
-            placeholder="Digite sua comida"
-            onChange={(e) => this.handleChange(e, "food")} />
-            <Button text="Login" onClick={this.handleClick} />            
-            {
-                this.UNSAFE_componentWillMount.state.listItem.map((item, index) => {
-                    return <p key={index}>{item.order} | {item.food}</p>
-                })
-            }
-            <Link to="Home">Home</Link>
+      <div className="menu">
+        <h1>Menu</h1>
+        <h2>Café da Manhã</h2>
+        {
+          menu.breakfast.map((produto, i) => {
+            return <div key={i}>
+              <div>{produto.item} - Valor: R${produto.preco}
+                <button className="orderButton" key={i} onClick={() => this.cliqueDaCompra(produto)}>ADD</button>
+              </div>
+            </div>
+          })
+        }
+        <h2>Almoço - Jantar</h2>
+        {
+          menu.lunch.map((produto, i) => {
+            return <div key={i}>
+              <div> {produto.item} - Valor: R${produto.preco} 
+                <button className="orderButton" key={i} onClick={() => this.cliqueDaCompra(produto)}>ADD</button>
+              </div>
+            </div>
+          })
+        }
+
+        <hr></hr>
+        <h1> Itens Comprados </h1>
+        {
+          this.state.comprar.map((produto, i) => {
+            return <div key={i}>
+              <p key={i}>{produto.item} / {produto.preco * produto.quantidade} /
+              {produto.quantidade} </p>
+              <button onClick={() => this.cliqueDeleta(produto)}>Excluir</button>
+            </div>
+          })
+        }
+        <hr></hr>
+        <h1> Total </h1>
+        <p> Valor Total: R${valorTotal}</p>
       </div>
-    )
+    );
   }
 }
 
-export default withFirebaseAuth({firebaseAppAuth,
-})(App);
+export default Salao;
