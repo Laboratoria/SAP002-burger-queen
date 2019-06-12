f8192fbf1705e299818401557c601dd15de7c7a6
Camilla <camilla.noba@gmail.com>
2019-06-12 17:32:31 -0300

    login e logout ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react"
 import firebase from './firebaseConfig'
-import LoginContainer from "./components/LoginContainer"
-import Home from "./components/Home"
+import Login from "./components/Login"
+import Hall from "./components/Hall"
+import Kitchen from "./components/Kitchen"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-class App extends  React.Component {
+class App extends React.Component {
+  
   constructor() {
     super();
     this.state = ({
@@ -21,16 +24,24 @@ class App extends  React.Component {
       if (user) {
         this.setState({ user: user.uid });
         localStorage.setItem('user', user.uid);
+
       } else {
         this.setState({ user: null });
         localStorage.removeItem('user');
       }
     });
   }
+
   render() {
     return (
-     <div>{this.state.user ? ( <Home/>) : ( <LoginContainer />)}</div>
-    )}
-    }
-
+      <>
+        <Router>
+          <Route exact path="/salao" component={Hall} />
+          <Route exact path="/cozinha" component={Kitchen} />
+          <Route exact path="/" component={Login} />
+        </Router>
+      </>
+    )
+  }
+}
 export default App
---
+++ src/components/Kitchen.js
@@ -0,0 +1,33 @@
+import React from "react"
+import firebase from '../firebaseConfig'
+import { Button } from 'react-bootstrap'
+
+class Kitchen extends React.Component {
+  constructor() {
+    super();
+    this.logout = this.logout.bind(this);
+  }
+
+  logout() {
+    firebase.auth().signOut()
+    .then(this.props.history.push(`/login`))
+  }
+
+  render() {
+    return (
+      <div>
+        <Button
+          variant="primary"
+          type="submit"
+          className="mt-3"
+          onClick={this.logout}
+          block>
+          cozinha
+</Button>
+      </div>
+
+    )
+  }
+}
+
+export default Kitchen
