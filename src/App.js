83d7a5e1340cec795699208a44442924f995098a
Ana Paula Lopes Araujo <anapaulalopesaraujo@MacBook-Pro-de-Ana.local>
2019-06-16 20:43:32 -0300

    inclusão de novos arquivos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React from 'react';
-import './App.css';
-import firebase from "./firebaseConfig";
+import React from 'react'
+import firebase from './firebaseConfig'
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>#partiuBurgerQueen</h1>
-        </header>
+        
       </div>
     );
   }
