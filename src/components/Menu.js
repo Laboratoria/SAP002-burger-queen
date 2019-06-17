8592a8a9c9643b6b1b863847b21ae876530f3748
aplaraujo <aplaraujo@gmail.com>
2019-06-17 17:02:11 -0300

    mais modificações

---
+++ src/components/Menu.js
@@ -0,0 +1,99 @@
+const menuData = [
+    {
+        waiter: '',
+        client: '',
+        category: ''
+    },
+    {
+        id: 1,
+        meal: 'Sanduíche de presunto e queijo',
+        price: 'R$ 10.00'
+    },
+    {
+        id: 2,
+        meal: 'Hamburger simples com carne bovina',
+        price: 'R$ 10.00'
+    },
+    {
+        id: 3,
+        meal: 'Hamburger simples com frango',
+        price: 'R$ 10.00'
+    },
+    {
+        id: 4,
+        meal: 'Hamburger simples vegetariano',
+        price: 'R$ 10.00'
+    },
+    {
+        id: 5,
+        meal: 'Hamburger duplo com carne bovina',
+        price: 'R$ 15.00'
+    },
+    {
+        id: 6,
+        meal: 'Hamburger duplo com frango',
+        price: 'R$ 15.00'
+    },
+    {
+        id: 7,
+        meal: 'Hamburger duplo vegetariano',
+        price: 'R$ 15.00'
+    },
+    {
+        id: 8,
+        beverage: 'Café americano',
+        price: 'R$ 5.00'
+    },
+    {
+        id: 9,
+        beverage: 'Café com leite',
+        price: 'R$ 7.00'
+    },
+    {
+        id: 10,
+        beverage: 'Água 500ml',
+        price: 'R$ 5.00'
+    },
+    {
+        id: 11,
+        beverage: 'Água 750ml',
+        price: 'R$ 7.00'
+    },
+    {
+        id: 12,
+        beverage: 'Bebida com gás 500ml',
+        price: 'R$ 7.00'
+    },
+    {
+        id: 13,
+        beverage: 'Bebida com gás 750ml',
+        price: 'R$ 10.00'
+    },
+    {
+        id: 14,
+        beverage: 'Suco de fruta natural',
+        price: 'R$ 7.00'
+    },
+    {
+        id: 15,
+        complement: 'Batata frita',
+        price: 'R$ 5.00'
+    },
+    {
+        id: 16,
+        complement: 'Anéis de cebola',
+        price: 'R$ 5.00'
+    },
+    {
+        id: 17,
+        option: 'Queijo',
+        price: 'R$ 1.00'
+    },
+    {
+        id: 18,
+        option: 'Ovo',
+        price: 'R$ 1.00'
+    }
+]
+
+export default menuData
--- src/components/Salon.js
+++ src/components/Salon.js
@@ -34,17 +34,74 @@ class Salon extends Component {
 
     render() {
         return (
-            <main className="page">
+            <main className="salon-page">
                 <div className="container">
                     <div className="salon-header">
                         <h1>Salão</h1>
                         <button onClick={this.logoutUser}>Sair</button>
                     </div>
+                    <form className="waiter-info">
+                        <label for="waiter-name">Nome do(a) garçom/garçonete</label>
+                        <input type="text" id="text" name="waiter" placeholder="Digite seu nome" />
+                        <label for="client">Nome do cliente</label>
+                        <input type="text" id="client" name="client" placeholder="Digite o nome do cliente"/>
+                        <label for="client">Pedido</label>
+                        <Application />
+                    </form>
                 </div>
             </main>
         )
     }
 }
+
+class Application extends React.Component {
+state = {
+    options: [
+        'Café Americano', 
+        'Café com Leite', 
+        'Sanduíche de presunto e queijo', 
+        'Suco de fruta natural', 
+        'Hamburguer simples com carne bovina', 
+        'Hamburguer simples com frango',
+        'Hamburguer simples vegetariano',
+        'Hamburguer duplo com carne bovina', 
+        'Hamburguer duplo com frango',
+        'Hamburguer duplo vegetariano',
+        'Batata frita',
+        'Anéis de cebola',
+        'Água 500ml',
+        'Água 750ml',
+        'Bebida com gás 500ml',
+        'Bebida com gás 750ml'
+    ],
+    filter: ""
+    }
+    handleFilter = (newFilter) => {
+    this.setState(() => ({
+        filter: newFilter
+    }));
+    };
+    render() {
+    const list = this.state.options.filter(option => option.toLowerCase().includes(this.state.filter.toLowerCase()))
+    return (
+    <div>
+        <Filter handleFilter={this.handleFilter} />
+        {list.map((option) => <div className="menu"><p className="menu-list">{option}<button className="order-button" onClick={this.openModal} style={{ cursor: "pointer" }}>Pedir</button></p></div>)}
+    </div>
+    );
+    };
+}  
+const Filter = (props) => (
+<div>
+    <input className="filter-list" name="filter" placeholder="Digite o pedido do cliente" onChange={(e) => {
+        props.handleFilter(e.target.value);
+        }}/>
+</div>
+);
+
+
+
+
 
 export default withFirebaseAuth({
     firebaseAppAuth,
