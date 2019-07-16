22fb3584c016cc22981455ed593152a8537978e9
TATIANE TABATA <tatit@br.ibm.com>
2019-07-15 23:16:04 -0300

    arrumando deploy - build

---
+++ build/precache-manifest.fb9da52d611a5ccc9ba9dcb760d01268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0dca7c7485eee399670840cc2b16107",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43aab09f87b4a0837ca1",
+    "url": "/static/css/main.a5c0ce81.chunk.css"
+  },
+  {
+    "revision": "4e7634729e33f2447449",
+    "url": "/static/js/2.b8416b0b.chunk.js"
+  },
+  {
+    "revision": "43aab09f87b4a0837ca1",
+    "url": "/static/js/main.07cd9374.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "85a8c91cb9bd1cd2d7a98b6540c05dfb",
+    "url": "/static/media/background.85a8c91c.jpg"
+  },
+  {
+    "revision": "b32dd0dce40dde372b5136f670406420",
+    "url": "/static/media/logo_pb.b32dd0dc.png"
+  }
+]);
